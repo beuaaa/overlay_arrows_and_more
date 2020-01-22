715954b72eaa2e3599677d0607bab109e3376cf9
David Pratmarty <david.pratmarty@oktal-se.fr>
2020-01-22 13:41:37 +0100

    1st commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='overlay_arrows_and_more',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/beuaaa/overlay_arrows_and_more',
+    license='MIT',
+    author='david pratmarty',
+    author_email='david.pratmarty@gmail.com',
+    description='overlay arrows and more'
+)

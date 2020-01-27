46478d652b96994178ec2dca949ea9d16eb20f1f
David Pratmarty <d_pratmarty@hotmail.com>
2020-01-27 11:39:36 +0100

    More documentation

--- overlay_arrows_and_more/overlay.py
+++ overlay_arrows_and_more/overlay.py
@@ -5,6 +5,71 @@ import win32con
 import win32gui
 import win32ui
 
+from threading import Thread
+
+from enum import Enum
+
+
+class Shape(Enum):
+	rectangle = 0
+	arrow = 1
+
+
+class TransparentWindow(Thread):
+	def __init__(self):
+		Thread.__init__(self)
+		self.h_window = None
+
+	def run(self):
+		h_instance = win32api.GetModuleHandle()
+		class_name = 'MyWindowClassName'
+		wnd_class = win32gui.WNDCLASS()
+
+		wnd_class.style = win32con.CS_HREDRAW | win32con.CS_VREDRAW
+		wnd_class.lpfnWndProc = wnd_proc
+		wnd_class.hInstance = h_instance
+		wnd_class.hCursor = win32gui.LoadCursor(None, win32con.IDC_ARROW)
+		wnd_class.hbrBackground = win32gui.GetStockObject(win32con.WHITE_BRUSH)
+		wnd_class.lpszClassName = class_name
+
+		wnd_class_atom = win32gui.RegisterClass(wnd_class)
+		ex_style = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | \
+				   win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
+		style = win32con.WS_DISABLED | win32con.WS_POPUP | win32con.WS_VISIBLE
+
+		self.h_window = win32gui.CreateWindowEx(
+			ex_style, wnd_class_atom,
+			'OverlayWindow',  # WindowName
+			style,
+			0,  # x
+			0,  # y
+			win32api.GetSystemMetrics(win32con.SM_CXSCREEN),  # width
+			win32api.GetSystemMetrics(win32con.SM_CYSCREEN),  # height
+			None,  # hWndParent
+			None,  # hMenu
+			h_instance,
+			None  # lpParam
+		)
+
+		win32gui.SetLayeredWindowAttributes(self.h_window, 0x00ffffff, 255, win32con.LWA_COLORKEY | win32con.LWA_ALPHA)
+		win32gui.SetWindowPos(self.h_window, win32con.HWND_TOPMOST, 0, 0, 0, 0,
+							  win32con.SWP_NOACTIVATE | win32con.SWP_NOMOVE | win32con.SWP_NOSIZE | win32con.SWP_SHOWWINDOW)
+
+		win32gui.PumpMessages()
+
+	def refresh(self):
+		win32gui.InvalidateRect(self.h_window, None, True)
+
+	def get_rectangles(self):
+		return self.rectangle_list
+
+
+rectangle_list = []
+
+def add(geometry=Shape.rectangle, x=0, y=0, width=22, height=22):
+	global rectangle_list
+	if geometry is Shape.rectangle:
+		rectangle_list.append([x, y, width, height])
 
 def wnd_proc(h_wnd, message, w_param, l_param):
 	"""Displays a transparent window with some graphic elements
@@ -15,6 +80,7 @@ def wnd_proc(h_wnd, message, w_param, l_param):
 
 	:returns: nothing
 	"""
+	global rectangle_list
 	if message == win32con.WM_PAINT:
 		hdc, paint_struct = win32gui.BeginPaint(h_wnd)
 
@@ -32,8 +98,14 @@ def wnd_proc(h_wnd, message, w_param, l_param):
 
 		try:
 			pen = win32gui.CreatePen(win32con.PS_GEOMETRIC, 5, win32api.RGB(255, 0, 0))
+
 			win32gui.SelectObject(hdc, pen)
 			win32gui.Ellipse(hdc, 10, 10, 40, 40)
+
+			for r in rectangle_list:
+				win32gui.Rectangle(hdc, r[0], r[1], r[0] + r[2], r[1] + r[3])
+
+
 
 		except:
 			# exc_type, exc_value, exc_traceback = sys.exc_info()
@@ -52,37 +124,8 @@ def wnd_proc(h_wnd, message, w_param, l_param):
 
 
 if __name__ == '__main__':
-	h_instance = win32api.GetModuleHandle()
-	className = 'MyWindowClassName'
-	wndClass = win32gui.WNDCLASS()
-
-	wndClass.style = win32con.CS_HREDRAW | win32con.CS_VREDRAW
-	wndClass.lpfnWndProc = wnd_proc
-	wndClass.hInstance = h_instance
-	wndClass.hCursor = win32gui.LoadCursor(None, win32con.IDC_ARROW)
-	wndClass.hbrBackground = win32gui.GetStockObject(win32con.WHITE_BRUSH)
-	wndClass.lpszClassName = className
-
-	wndClassAtom = win32gui.RegisterClass(wndClass)
-	exStyle = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
-	style = win32con.WS_DISABLED | win32con.WS_POPUP | win32con.WS_VISIBLE
-
-	h_window = win32gui.CreateWindowEx(
-		exStyle, wndClassAtom,
-		'OverlayWindow',  # WindowName
-		style,
-		0,  # x
-		0,  # y
-		win32api.GetSystemMetrics(win32con.SM_CXSCREEN),  # width
-		win32api.GetSystemMetrics(win32con.SM_CYSCREEN),  # height
-		None,  # hWndParent
-		None,  # hMenu
-		h_instance,
-		None  # lpParam
-	)
-
-	win32gui.SetLayeredWindowAttributes(h_window, 0x00ffffff, 255, win32con.LWA_COLORKEY | win32con.LWA_ALPHA)
-	win32gui.SetWindowPos(h_window, win32con.HWND_TOPMOST, 0, 0, 0, 0,
-						  win32con.SWP_NOACTIVATE | win32con.SWP_NOMOVE | win32con.SWP_NOSIZE | win32con.SWP_SHOWWINDOW)
-
-	win32gui.PumpMessages()
+	transparent_window = TransparentWindow()
+	transparent_window.start()
+	add(geometry=Shape.rectangle, x=100, y=100, width=100, height=100)
+	add(geometry=Shape.rectangle, x=300, y=100, width=100, height=100)
+	transparent_window.refresh()

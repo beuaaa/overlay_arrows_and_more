807a9b303abde2c3f180a73a38064fbc6c8d987c
David Pratmarty <david.pratmarty@oktal-se.fr>
2020-01-27 15:45:56 +0100

    New v0.1.1

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='overlay_arrows_and_more',
-    version='0.1',
+    version='0.1.1',
     packages=['overlay_arrows_and_more'],
     url='https://github.com/beuaaa/overlay_arrows_and_more',
     license='MIT',
     author='david pratmarty',
     author_email='david.pratmarty@gmail.com',
-    description='overlay arrows and more', install_requires=['pypiwin32']
+    description='overlay arrows and more', install_requires=['']
 )

89951b8dee668965b30b3b98a823f622166ac92f
David Pratmarty <d_pratmarty@hotmail.com>
2020-05-20 09:41:00 +0200

    Python 3.8 is now supported

--- overlay_arrows_and_more/__init__.py
+++ overlay_arrows_and_more/__init__.py
@@ -4,6 +4,6 @@
     To be documented
 """
 
-__version__ = "0.2.5"
+__version__ = "0.2.7"
 
 from overlay import *
--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 
 import overlay_arrows_and_more
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='overlay_arrows_and_more',
     version= overlay_arrows_and_more.__version__,
@@ -10,5 +13,22 @@ setup(
     license='MIT',
     author='david pratmarty',
     author_email='david.pratmarty@gmail.com',
-    description='overlay arrows and more', install_requires=['']
+    description='overlay arrows and more',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    install_requires=[''],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Software Development :: Testing',
+    ]
 )
